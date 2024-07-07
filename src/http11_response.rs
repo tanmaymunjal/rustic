869ac33f9fe7c509b3b48e4743c162765616e114
Tanmay Munjal <tanmaymunjal64@gmail.com>
2024-07-07 21:02:53 +0530

    Added docs

--- src/http11_response.rs
+++ src/http11_response.rs
@@ -4,6 +4,7 @@ use std::net::TcpStream;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Clone)]
+/// Represents an HTTP response sent by the server.
 pub struct Response<'a> {
     pub status_code: u16,
     pub reason: &'a str,
@@ -11,6 +12,21 @@ pub struct Response<'a> {
     pub headers: HashMap<String, String>,
 }
 
+/// Retrieves the current date and time in UTC format as a string.
+///
+/// This function uses the system's current time and formats it
+/// according to the HTTP-date specification.
+///
+/// # Returns
+///
+/// * `String` - The current date and time in UTC format.
+///
+/// # Examples
+///
+/// ```
+/// let date = get_current_utc_date();
+/// println!("{}", date); // Example: "Sun, 07 Jul 2024 12:00:00 GMT"
+/// ```
 fn get_current_utc_date() -> String {
     let now = SystemTime::now();
     let seconds_since_epoch = now.duration_since(UNIX_EPOCH).unwrap().as_secs();
@@ -23,11 +39,50 @@ fn get_current_utc_date() -> String {
         .to_string()
 }
 
-pub fn write_status_header(status_code: u16, reason: &str) -> String {
+/// Constructs the status line for an HTTP response.
+///
+/// This function formats the HTTP status line based on the provided status code and reason phrase.
+///
+/// # Arguments
+///
+/// * `status_code` - The HTTP status code.
+/// * `reason` - The reason phrase associated with the status code.
+///
+/// # Returns
+///
+/// * `String` - The formatted HTTP status line.
+///
+/// # Examples
+///
+/// ```
+/// let status_line = write_status_header(200, "OK");
+/// assert_eq!(status_line, "HTTP/1.1 200 OK \r\n");
+/// ```
+fn write_status_header(status_code: u16, reason: &str) -> String {
     format!("HTTP/1.1 {} {} \r\n", status_code, reason)
 }
 
-pub fn write_header(headers: &mut HashMap<String, String>) -> String {
+/// Constructs the HTTP headers from a given `HashMap`.
+///
+/// This function formats the HTTP headers and adds the current date if not already present.
+///
+/// # Arguments
+///
+/// * `headers` - A mutable reference to a `HashMap` containing the headers.
+///
+/// # Returns
+///
+/// * `String` - The formatted HTTP headers.
+///
+/// # Examples
+///
+/// ```
+/// let mut headers = HashMap::new();
+/// headers.insert("Content-Type".to_string(), "text/plain".to_string());
+/// let headers_string = write_header(&mut headers);
+/// assert!(headers_string.contains("Content-Type: text/plain\r\n"));
+/// ```
+fn write_header(headers: &mut HashMap<String, String>) -> String {
     headers.insert("Date".to_string(), get_current_utc_date());
 
     let mut header_string = String::new();
@@ -38,6 +93,27 @@ pub fn write_header(headers: &mut HashMap<String, String>) -> String {
     header_string
 }
 
+/// Writes an HTTP response to the given TCP stream.
+///
+/// This function writes the status line, headers, and optionally the response body to the TCP stream.
+///
+/// # Arguments
+///
+/// * `stream` - A mutable reference to the `TcpStream`.
+/// * `response` - The HTTP response to be written.
+///
+/// # Examples
+///
+/// ```
+/// let mut stream = TcpStream::connect("127.0.0.1:8080").unwrap();
+/// let response = Response {
+///     status_code: 200,
+///     reason: "OK",
+///     response_body: Some("Hello, world!"),
+///     headers: HashMap::new(),
+/// };
+/// write_connection(&mut stream, response);
+/// ```
 pub fn write_connection(stream: &mut TcpStream, mut response: Response) {
     let status_line = write_status_header(response.status_code, response.reason);
     let headers_string = write_header(&mut response.headers);
@@ -50,6 +126,26 @@ pub fn write_connection(stream: &mut TcpStream, mut response: Response) {
     stream.write_all(full_response.as_bytes()).unwrap();
 }
 
+/// Converts a `HashMap` to a JSON string.
+///
+/// This function formats a `HashMap` as a JSON string.
+///
+/// # Arguments
+///
+/// * `map` - A reference to the `HashMap` to be converted.
+///
+/// # Returns
+///
+/// * `String` - The formatted JSON string.
+///
+/// # Examples
+///
+/// ```
+/// let mut map = HashMap::new();
+/// map.insert("key1", "value1");
+/// let json_data = hashmap_to_json(&map);
+/// assert_eq!(json_data, "{\"key1\": \"value1\"}");
+/// ```
 pub fn hashmap_to_json<K: std::fmt::Display, V: std::fmt::Display>(map: &HashMap<K, V>) -> String {
     let mut json_string = String::from("{");
 
@@ -60,7 +156,7 @@ pub fn hashmap_to_json<K: std::fmt::Display, V: std::fmt::Display>(map: &HashMap
         }
     }
 
-    json_string.push_str("}");
+    json_string.push('}');
 
     json_string
 }
@@ -69,12 +165,14 @@ pub fn hashmap_to_json<K: std::fmt::Display, V: std::fmt::Display>(map: &HashMap
 mod test_http_response_functions {
     use super::*;
 
+    /// Tests the `write_status_header` function.
     #[test]
     fn test_write_status_header() {
         let status_line = write_status_header(200, "OK");
         assert_eq!(status_line, "HTTP/1.1 200 OK \r\n");
     }
 
+    /// Tests the `write_header` function.
     #[test]
     fn test_write_header() {
         let mut headers = HashMap::new();
@@ -86,6 +184,7 @@ mod test_http_response_functions {
         assert!(header_string.contains("Content-Length: 123\r\n"));
     }
 
+    /// Tests the `hashmap_to_json` function.
     #[test]
     fn test_hashmap_to_json() {
         let mut map = HashMap::new();
--- src/parse_url.rs
+++ src/parse_url.rs
@@ -1,14 +1,36 @@
 use std::collections::HashMap;
 
-
+/// Parses URL parameters from a given URL string and returns them as a `HashMap<String, String>`.
+///
+/// # Arguments
+///
+/// * `url` - A string slice representing the URL containing parameters.
+///
+/// # Returns
+///
+/// A `HashMap` where each key-value pair corresponds to a parsed parameter from the URL.
+///
+/// # Examples
+///
+/// ```
+/// use crate::parse_url_param;
+/// use std::collections::HashMap;
+///
+/// let url = "http://example.com/?key=value";
+/// let result = parse_url_param(url);
+/// let mut expected = HashMap::new();
+/// expected.insert("key".to_string(), "value".to_string());
+/// assert_eq!(result, expected);
+/// ```
 pub fn parse_url_param(url: &str) -> HashMap<String, String> {
+    // Split the URL at the '?' character to isolate parameters part.
     url.split_once('?')
-        .map(|(_, params)| params)
+        .map(|(_, params)| params) // Take the parameters part or an empty string if no '?'
         .unwrap_or("")
-        .split('&')
-        .filter_map(|s| s.split_once('='))
-        .map(|(k, v)| (k.to_string(), v.to_string()))
-        .collect()
+        .split('&') // Split parameters into key-value pairs
+        .filter_map(|s| s.split_once('=')) // Filter out invalid pairs and split into (key, value)
+        .map(|(k, v)| (k.to_string(), v.to_string())) // Convert (key, value) pairs into (String, String)
+        .collect() // Collect into a HashMap<String, String>
 }
 
 #[cfg(test)]
