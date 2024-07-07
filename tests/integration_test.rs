b2a08a2e82943384d58dd2bb1d1869243e5ef80b
Tanmay Munjal <tanmaymunjal64@gmail.com>
2024-07-07 19:35:55 +0530

    Got a basic api working

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,12 @@
 #[cfg(test)]
 mod integration_tests {
     use reqwest::blocking::Client;
+    use rustic::app::{run, App, Request};
     use rustic::connection::{handle_connection, listen_at_port};
+    use rustic::parse_headers::RequestType;
     use std::sync::mpsc;
     use std::thread;
+    use std::time::Duration;
 
     #[test]
     fn test_port_bind() {
@@ -12,24 +15,24 @@ mod integration_tests {
 
     #[test]
     fn test_request() {
-        let listener = listen_at_port(8000);
+        let listener = listen_at_port(8001);
         let (tx, rx) = mpsc::channel();
 
         let handle = thread::spawn(move || {
-            if let Ok((stream, _)) = listener.accept() {
-                let request = handle_connection(stream);
+            if let Ok((mut stream, _)) = listener.accept() {
+                let request = handle_connection(&mut stream);
                 tx.send(request).unwrap();
             }
         });
 
         // Send a request
         let client = Client::new();
-        let url = "http://localhost:8000/";
+        let url = "http://localhost:8001/";
         let _ = client.get(url).send();
 
         // Wait to receive the processed request
         let received_request = rx
-            .recv_timeout(std::time::Duration::from_secs(2))
+            .recv_timeout(Duration::from_secs(2))
             .expect("Didn't receive a request within the timeout period");
 
         // Assert that we received the expected request
@@ -46,11 +49,21 @@ mod integration_tests {
             "The second line should be accept header"
         );
         assert_eq!(
-            received_request[2], "host: localhost:8000",
+            received_request[2], "host: localhost:8001",
             "The third line should be host header"
         );
 
         // Wait for the listener thread to finish
         handle.join().unwrap();
+    }
+
+    #[test]
+    fn test_create_app() {
+        let mut application = App::new();
+        fn hello_world(_: Request) -> Option<String> {
+            Some("HTTP/1.1 200 Hello".to_string())
+        }
+        application.add_endpoint("test", RequestType::GET, hello_world);
+        run(application, 8002, true);
     }
 }
