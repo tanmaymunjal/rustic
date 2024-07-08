ea8ef641d8a3a443c00801eefdfe84487d7c63a2
Tanmay Munjal <72982935+tanmaymunjal@users.noreply.github.com>
2024-07-08 13:42:42 +0530

    Added auto update doc github action
    * Added auto update doc github action
    
    * test this
    
    * test this
    
    * test this
    
    * Test this
    
    * Add generated docs
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- docs/trait.impl/core/clone/trait.Clone.js
+++ docs/trait.impl/core/clone/trait.Clone.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rustic":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"rustic/http11_response/struct.Response.html\" title=\"struct rustic::http11_response::Response\">Response</a>&lt;'a&gt;"]]
+"rustic":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"rustic/http11_response/struct.Response.html\" title=\"struct rustic::http11_response::Response\">Response</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- generate_docs.sh
+++ generate_docs.sh
@@ -2,3 +2,5 @@ cargo doc --no-deps
 rm -rf ./docs
 echo "<meta http-equiv=\"refresh\" content=\"0; url=rustic\">" > target/doc/index.html
 cp -r target/doc ./docs
+cargo install cargo-tarpaulin
+cargo tarpaulin --out Html
